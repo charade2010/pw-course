7f8b4f88d682bc8faaa582fb71357502bede89f5
K6-Nguyen Minh Khang <charade2010@gmail.com>
2024-05-20 21:09:46 +0700

    chore: Exercises_Day 2

--- git-course/lesson-2/ex7.js
+++ git-course/lesson-2/ex7.js
@@ -1,5 +1,6 @@
+console.log(`Cac gia tri chia het cho 3: `);
 for (let i = 1000; i <= 2000; i++) {
     if (i % 3 === 0) {
-        console.log(`Cac gia tri chia het cho 3: `, i);
+        console.log(i);
     }
 };
--- git-course/lesson-2/ex8.js
+++ git-course/lesson-2/ex8.js
@@ -1,3 +1,5 @@
-for (let i = 1; i <= 100; i+4) {
-    console.log("Cac gia tri cach nhau 4 don vi tu 1 -> 100: ", i + 4);
+console.log("Cac gia tri cach nhau 4 don vi tu 1 -> 100: ");
+for (let i = 1; i <= 100;) {
+    console.log(i);
+    i+=4;
 };
