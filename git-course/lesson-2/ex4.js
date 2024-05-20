0a68f33b51524447f4491a768c5f771c242218cf
K6-Nguyen Minh Khang <charade2010@gmail.com>
2024-05-20 07:45:11 +0700

    chore:Exercise_Day2

---
+++ git-course/lesson-2/ex4.js
@@ -0,0 +1,5 @@
+let height = 168;
+let weightideal = (height%100)*9/10;
+let weightmin = (height%100)*8/10;
+let weighmax = (height%100);
+console.log('Can nang ly tuong: ',weightideal,'Can nang toi da: ',weighmax,' Can nang toi thieu: ',weightmin);
---
+++ git-course/lesson-2/ex8.js
@@ -0,0 +1,3 @@
+for (let i = 1; i <= 100; i+4) {
+    console.log("Cac gia tri cach nhau 4 don vi tu 1 -> 100: ", i + 4);
+};
